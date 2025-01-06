30107fa1d0f2400eebb8449c5700694c57d96de9
Jang <hjjang501@gmail.com>
2025-01-06 16:31:12 +0900

    Initialize project with spring initializr

---
+++ pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/Pt2ImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.github.hojoungjang.pt2_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pt2ImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pt2ImplementationApplication.class, args);
+	}
+
+}
---
+++ pt2-implementation/src/test/java/com/github/hojoungjang/pt2_implementation/Pt2ImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hojoungjang.pt2_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pt2ImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
