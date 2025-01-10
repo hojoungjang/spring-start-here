2a1b37e7af3dcb94b1409d68392f256ab893b8f2
Jang <hjjang501@gmail.com>
2025-01-10 15:22:45 +0900

    Use @RequestBody to read data from request body

--- pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
+++ pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
@@ -1,8 +1,11 @@
 package com.github.hojoungjang.pt2_implementation.controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.hojoungjang.pt2_implementation.model.PaymentDetails;
@@ -12,13 +15,17 @@ import com.github.hojoungjang.pt2_implementation.service.PaymentService;
 public class PaymentController {
     private final PaymentService paymentService;
 
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(
+        @RequestBody PaymentDetails paymentDetails
+    ) {
+        logger.info("Received payment " + paymentDetails.getAmount());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
     }
 }
