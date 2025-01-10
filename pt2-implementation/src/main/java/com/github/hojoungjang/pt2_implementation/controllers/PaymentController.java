3182fda01ca378b6b1a46c816b35b5475c307195
Jang <hjjang501@gmail.com>
2025-01-10 15:15:06 +0900

    Use rest controller advice to handle exceptions

--- pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
+++ pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.hojoungjang.pt2_implementation.exception.NotEnoughMoneyException;
-import com.github.hojoungjang.pt2_implementation.model.ErrorDetails;
 import com.github.hojoungjang.pt2_implementation.model.PaymentDetails;
 import com.github.hojoungjang.pt2_implementation.service.PaymentService;
 
@@ -19,14 +17,8 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make a payment.");
-            return ResponseEntity.badRequest().body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
     }
 }
