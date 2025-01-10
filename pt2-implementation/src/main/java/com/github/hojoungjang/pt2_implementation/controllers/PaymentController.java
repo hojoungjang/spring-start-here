3ae47d6e3d979b48d81f3b0526c9c617768e4068
Jang <hjjang501@gmail.com>
2025-01-10 17:43:31 +0900

    Use RestTemplate to make API calls to REST endpoints

--- pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
+++ pt2-implementation/src/main/java/com/github/hojoungjang/pt2_implementation/controllers/PaymentController.java
@@ -9,14 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.hojoungjang.pt2_implementation.model.Payment;
 import com.github.hojoungjang.pt2_implementation.proxy.PaymentsProxy;
+import com.github.hojoungjang.pt2_implementation.proxy.PaymentsProxyRestTemplate;
 
 @RestController
 public class PaymentController {
     private final PaymentsProxy paymentsProxy;
+    private final PaymentsProxyRestTemplate paymentsProxyRestTemplate;
     private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
-    public PaymentController(PaymentsProxy paymentsProxy) {
+    public PaymentController(
+        PaymentsProxy paymentsProxy,
+        PaymentsProxyRestTemplate paymentsProxyRestTemplate
+    ) {
         this.paymentsProxy = paymentsProxy;
+        this.paymentsProxyRestTemplate = paymentsProxyRestTemplate;
     }
 
     @PostMapping("/payment")
@@ -26,5 +32,13 @@ public class PaymentController {
         logger.info("received payment; amount: " + payment.getAmount());
         String requestId = UUID.randomUUID().toString();
         return paymentsProxy.createPayment(requestId, payment);
+    }
+
+    @PostMapping("/payment-rest-template")
+    public Payment makePaymentRestTemplate(
+        @RequestBody Payment payment
+    ) {
+        logger.info("received payment; amount: " + payment.getAmount());
+        return paymentsProxyRestTemplate.createPayment(payment);
     }
 }
